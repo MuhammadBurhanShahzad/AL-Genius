85210b5b1e08966c2f6a69702c641abacbf85c88
Muhammad Burhan Shahzad <burishahzada7728@gmail.com>
2024-09-15 10:40:47 +0500

    initial commit

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SignIn } from '@clerk/nextjs';
+
+const SingInPage = () => {
+  return <SignIn /> 
+}
+
+export default SingInPage
---
+++ app/(auth)/sign-up/[[...sign up]]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { SignUp } from '@clerk/nextjs';
+const SingUpPage = () => {
+  return <SignUp /> 
+}
+
+export default SingUpPage
